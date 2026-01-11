2c93a529d5b62e687ce3fc88baa9ba3f236bbac4
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-11 14:17:47 +0100

    feat: aside panel created with brut data

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Panel() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <aside className="h-screen flex flex-col gap-2 bg-teal-200 max-w-[250px] p-4">
+            <h2 className="font-semibold">Chats</h2>
+            <div>
+                <h2>Titre</h2>
+                <h2>Titre</h2>
+                <h2>Titre</h2>
+                <h2>Titre</h2>
+                <h2>Titre</h2>
+                <h2>Titre</h2>
+            </div>
+        </aside>
+    )
 }
