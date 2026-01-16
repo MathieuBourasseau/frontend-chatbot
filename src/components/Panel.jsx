2697fb471da5b478e06f211d289375b57f7136a4
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-16 13:38:08 +0100

    feat: added these new props to Panels.jsx and Chat.jsx

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -6,7 +6,7 @@ import { CiCirclePlus } from "react-icons/ci";
 import PanelButton from "./PanelButton";
 
 
-export default function Panel({setCurrentChatId}) {
+export default function Panel({setCurrentChatId, chatsList, setChatsList}) {
 
     // Menu is closed by default
     const [isOpen, setIsOpen] = useState(false);
