abfa562727843f4bcb351e5745a968e22d726ebc
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-22 13:51:51 +0100

    feat: font size added to register button

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -73,7 +73,7 @@ export default function AuthForm() {
 
                     {/* REGISTER */}
                     <button
-                        className="text-white"
+                        className="text-white text-sm"
                         type="button"
                     >
                         Pas encore de compte ? {''} 
