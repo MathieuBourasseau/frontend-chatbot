503bd3c7283d950aa555ffc140d8bd48ff311023
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-23 20:14:13 +0100

    feat: toggle function created + isOpen state created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
-import PanelButton from "./PanelButton";
+import { ImCross } from "react-icons/im";
 
 export default function Header() {
-  return (
-    <header className="p-4">
 
-        {/* MOBILE NAV */}
-        <nav 
-            className="flex items-center justify-between lg:hidden">
-            <PanelButton
-            
-            />
-            <h1 className="font-bold">Projet LLM</h1>
-            <span>user data</span>
-        </nav>
-    </header>
-  )
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <header className="p-4">
+
+            {/* MOBILE NAV */}
+            <nav
+                className="flex items-center justify-between lg:hidden">
+                <button
+                    onClick={toggleMenu}
+                    className="cursor-pointer"
+                >  
+                    {isOpen ? <ImCross className="text-[#003C57]" /> : <GiHamburgerMenu className="text-[#003C57]" />  }
+                </button>
+                <h1 className="font-bold">Projet LLM</h1>
+                <span>user data</span>
+            </nav>
+        </header>
+    )
 }
