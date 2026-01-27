a1da9cb161c2ae8492864ff7ce0db8562052334c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-27 11:37:34 +0100

    feat: disconnection desktop created in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-import { ImCross } from "react-icons/im";
+import { CiLogout } from "react-icons/ci";
 
 export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, user }) {
 
@@ -27,7 +27,7 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
             </nav>
 
             {/* DESKTOP NAV */}
-            <nav className="hidden lg:block lg:flex lg:justify-between lg:items-center">
+            <nav className="hidden lg:block lg:flex lg:justify-between lg:items-center lg:relative">
                 <h1 className="text-[#003c57]">Projet portfolio LLM</h1>
 
                 {/* SHOW CHAT TITLE IF EXISTING */}
@@ -38,17 +38,29 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
                 {/* SHOW USER AVATAR */}
                 {user.avatar ? (
                     <span
-                        className="w-10 h-10 rounded-full flex items-center justify-center border border-[#003C57] shadow-sm"
+                        className="cursor-pointer w-10 h-10 rounded-full flex items-center justify-center border border-[#003C57] shadow-sm"
                     >
                         <img src={user.avatar} alt="Avatar de l'utilisateur"/>
                     </span>
                 ) : (
                     <span
-                        className="w-10 h-10 rounded-full font-bold flex items-center justify-center border-3 border-[#003C57] shadow-sm"
+                        className="cursor-pointer w-10 h-10 rounded-full font-bold flex items-center justify-center border-3 border-[#003C57] shadow-sm"
                     >
                         {user.username[0].toUpperCase()}
                     </span>
                 )}
+
+                {/* DISCONNECTION DESKTOP */}
+                <div
+                    className="bg-[#003C57] p-2 absolute lg:right-0 lg:bottom-[-50px] rounded-lg text-white"
+                >
+                    <button
+                        className="flex items-center gap-2"
+                    >
+                        Déconnexion
+                        <span><CiLogout /></span>
+                    </button>
+                </div>
             </nav>
         </header>
     )
