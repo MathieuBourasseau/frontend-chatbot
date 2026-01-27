72c28aa79f78bf5b3354d7573a54430b166fc9b6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-27 15:25:31 +0100

    feat: useEffect to check token added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,44 @@ function App() {
   const [user, setUser] = useState(null); // No user by default
   const [isOpen, setIsOpen] = useState(false);  // Menu is closed by default
 
+  // --- VERIFY IF A TOKEN EXISTS AND IF IT IS NOT EXPIRED ---
+  useEffect(() => {
+
+    const checkToken = async () => {
+
+      // Read token save in local storage
+      const token = localStorage.getItem('token');
+
+      // Stop here if token does not exist
+      if (!token) return;
+
+      // Fetch to verify the token
+      try {
+
+        const response = await fetch('http://localhost:3001/api/me', {
+          method: 'GET',
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        });
+
+        // 
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data.user);
+        } else {
+          localStorage.removeItem('token');
+        }
+
+      } catch (error) {
+        console.error("Impossible de contacter le serveur pour vérifier le token", error);
+      }
+    }
+
+    checkToken();
+  }, [])
+
+
   // --- SHOW ALL THE CHATS HISTORY ---
   useEffect(() => {
 
@@ -44,28 +82,28 @@ function App() {
 
       <Routes>
         {!user ? (
-        <>
-          <Route path="/login" element={<AuthForm setUser={setUser} />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </>
+          <>
+            <Route path="/login" element={<AuthForm setUser={setUser} />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
         ) : (
-        <>
-          <Route
-            path="/chats"
-            element={
-              <>
-                <Panel setCurrentChatId={setCurrentChatId} chatsList={chatsList} setChatsList={setChatsList} user={user} isOpen={isOpen} setIsOpen={setIsOpen} />
-                <div className={`transition-transform duration-300 bg-white flex flex-col flex-1 h-full z-10 ${isOpen ? "translate-x-[350px]" : "translate-x-0"} lg:translate-x-0`}>
-                  <Header isOpen={isOpen} setIsOpen={setIsOpen} currentChatId={currentChatId} chatsList={chatsList} user={user} setUser={setUser} />
-                  <div className="flex flex-1 overflow-hidden relative">
-                    <Chat currentChatId={currentChatId} setCurrentChatId={setCurrentChatId} setChatsList={setChatsList} chatsList={chatsList} user={user} />
+          <>
+            <Route
+              path="/chats"
+              element={
+                <>
+                  <Panel setCurrentChatId={setCurrentChatId} chatsList={chatsList} setChatsList={setChatsList} user={user} isOpen={isOpen} setIsOpen={setIsOpen} />
+                  <div className={`transition-transform duration-300 bg-white flex flex-col flex-1 h-full z-10 ${isOpen ? "translate-x-[350px]" : "translate-x-0"} lg:translate-x-0`}>
+                    <Header isOpen={isOpen} setIsOpen={setIsOpen} currentChatId={currentChatId} chatsList={chatsList} user={user} setUser={setUser} />
+                    <div className="flex flex-1 overflow-hidden relative">
+                      <Chat currentChatId={currentChatId} setCurrentChatId={setCurrentChatId} setChatsList={setChatsList} chatsList={chatsList} user={user} />
+                    </div>
                   </div>
-                </div>
-              </>
-            }
-          />
-          <Route path="*" element={ <Navigate to="/chats"/> } />
-        </>
+                </>
+              }
+            />
+            <Route path="*" element={<Navigate to="/chats" />} />
+          </>
         )}
       </Routes>
     </div>
